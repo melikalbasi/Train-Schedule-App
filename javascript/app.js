2090d3e0ff0750fbe751233cbf6c8d7f3847576f
Melika Kalbasi <melika_kalbasi@melikas-air.gateway.sonic.net>
2018-11-26 19:44:42 -0800

    onclick function

--- javascript/app.js
+++ javascript/app.js
@@ -19,13 +19,48 @@ console.log(database);
 $("#submit-button").on("click", function(event){
     // prevent default behavior of refresh page
     event.preventDefault();
-    
 
+    //assign variable for values from user inputs for train-name, destination, first-train, and frequency
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#first-train").val().trim();
+    var frequency = $("#frequency").val().trim();
 
+    //log variables to ensure we are grabbing info from form
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrain);
+    console.log(frequency);
 
+    // Push creates a new 'child'/'branch' in the root directory of your database. The new child/branch will be given an auto-generated serial number, and each of the key-value pairs defined below will be pushed into that new child/branch.
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
 
-})
+});
 
+// This chunk of code can be triggered in two scenarios: 1.) On page load/refesh this code will be executed FOR EACH child/branch that exists in the root directory of the database. 2.) This code will be executed when a new child/branch is added to the database (which, on this page, happens each time the click listener is triggered on the submit button).
+database.ref().on("child_added", function(childSnapshot){
+    var trainName = $("<td>").text(childSnapshot.val().trainName);
+    var destination = $("<td>").text(childSnapshot.val().destination);
+    var firstTrain = $("<td>").text(childSnapshot.val().firstTrain);
+    var frequency = $("<td>").text(childSnapshot.val().frequency);
+    var nextArrival = $("<td>").text("");
+    var minutesAway = $("<td>").text("");
+
+    var newRow = $("<tr>");
+
+    newRow.append(trainName, destination, frequency, nextArrival, minutesAway);
+
+    $("#schedule").append(newRow);
+
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
 
 // ===== MATH FOR TRAIN TIME ===== //
 //get train start from the database
