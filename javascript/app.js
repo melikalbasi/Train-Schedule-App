508628991d135789c05233ad831d5e1fa844c578
Melika Kalbasi <melika_kalbasi@melikas-air.gateway.sonic.net>
2018-11-27 18:11:42 -0800

    finish js

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,22 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAXOR2O3dum1umrzFO8jNgvTqvVMCj3TrM",
     authDomain: "train-schedule-app-5caa2.firebaseapp.com",
     databaseURL: "https://train-schedule-app-5caa2.firebaseio.com",
     projectId: "train-schedule-app-5caa2",
     storageBucket: "train-schedule-app-5caa2.appspot.com",
     messagingSenderId: "708092872162"
-  };
+};
 
 // Pass Firebase credentials (above) into initializeApp method
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 // assign variable to firebase.database()
 var database = firebase.database()
 console.log(database);
 
 // click listener for submit button
-$("#submit-button").on("click", function(event){
+$("#submit-button").on("click", function (event) {
     // prevent default behavior of refresh page
     event.preventDefault();
 
@@ -41,16 +41,51 @@ $("#submit-button").on("click", function(event){
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train").val("");
+    $("#frequency").val("");
+
 });
 
 // This chunk of code can be triggered in two scenarios: 1.) On page load/refesh this code will be executed FOR EACH child/branch that exists in the root directory of the database. 2.) This code will be executed when a new child/branch is added to the database (which, on this page, happens each time the click listener is triggered on the submit button).
-database.ref().on("child_added", function(childSnapshot){
+database.ref().on("child_added", function (childSnapshot) {
+    var nextArrival;
+
+    //get train start from the database
+    console.log(childSnapshot.val().firstTrain);
+    var trainStart = childSnapshot.val().firstTrain;
+    console.log(trainStart);
+
+    // Pushing the start time back 1 yr to ensure it comes before the current time
+    var trainStartConv = moment(trainStart, "HH:mm").subtract(1, 'years');
+    console.log(trainStartConv)
+
+    // calculate the difference between trainStart and curTime
+    var timeDiff = moment().diff(moment(trainStartConv), "minutes");
+    console.log(timeDiff);
+
+    // calculate time apart
+    var timeApart = timeDiff % childSnapshot.val().frequency;
+    console.log(timeApart);
+
+    // minutes until arrival
+    var minutesAway = childSnapshot.val().frequency - timeApart;
+    console.log(minutesAway);
+
+
+    //adding minutes until the train to the current time and formatting the appearance of the time
+    nextArrival = moment().add(minutesAway, "m").format("HH:mm");
+    console.log(nextArrival)
+
+    // add new row to on-screen table
+
     var trainName = $("<td>").text(childSnapshot.val().trainName);
     var destination = $("<td>").text(childSnapshot.val().destination);
     var firstTrain = $("<td>").text(childSnapshot.val().firstTrain);
     var frequency = $("<td>").text(childSnapshot.val().frequency);
-    var nextArrival = $("<td>").text("");
-    var minutesAway = $("<td>").text("");
+    var nextArrival = $("<td>").text(nextArrival);
+    var minutesAway = $("<td>").text(minutesAway);
 
     var newRow = $("<tr>");
 
@@ -58,27 +93,6 @@ database.ref().on("child_added", function(childSnapshot){
 
     $("#schedule").append(newRow);
 
-}, function(errorObject) {
+}, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
-
-// ===== MATH FOR TRAIN TIME ===== //
-//get train start from the database
-
-
-// Pushing the start time back 1 yr to ensure it comes before the current time
-
-
-// calculate the difference between trainStart and curTime
-
-
-// calculate time apart
-
-
-// minutes until arrival
-
-
-//adding minutes until the train to the current time and formatting the appearance of the time
-
-
-// add new row to on-screen table
